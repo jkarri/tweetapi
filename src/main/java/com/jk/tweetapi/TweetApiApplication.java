4bfa3fb6d4c02d7ff2bfa239e80a0bfbcc77be49
janardhan.karri <janakarri@gmail.com>
2017-02-11 22:50:11 +0000

    Initial project setup

---
+++ src/main/java/com/jk/tweetapi/TweetApiApplication.java
@@ -0,0 +1,12 @@
+package com.jk.tweetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetApiApplication.class, args);
+	}
+}
