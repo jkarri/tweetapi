50bdeb686d2690ff697eea3a2927423653641e4d
janardhan.karri <janakarri@gmail.com>
2017-02-12 00:38:23 +0000

    make inmomeory tweet service thread safe by using reentrant readwrite locks

--- src/main/java/com/jk/tweetapi/service/InMemoryTweetService.java
+++ src/main/java/com/jk/tweetapi/service/InMemoryTweetService.java
@@ -7,57 +7,93 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 
 import com.jk.tweetapi.domain.Tweet;
 import com.jk.tweetapi.domain.TweetFeed;
 
+/**
+ * Make this class thread safe using {@link ReentrantReadWriteLock}s.
+ * Write locks protect writes to the map and read locks to protect reads.
+ */
 public class InMemoryTweetService implements TweetService {
     private Map<String, Set<Tweet>> userTweets = new HashMap<>();
     private Map<String, Set<String>> userFollowers = new HashMap<>();
 
+    private ReadWriteLock tweetsLock = new ReentrantReadWriteLock();
+    private ReadWriteLock followersLock = new ReentrantReadWriteLock();
+
     public TweetFeed tweetsByUser(String userId) {
-        TweetFeed tweetFeed = new TweetFeed();
-        if (userTweets.get(userId) != null) {
-            tweetFeed.setTweets(new ArrayList<>(userTweets.get(userId)));
+        tweetsLock.readLock().lock();
+        try {
+            TweetFeed tweetFeed = new TweetFeed();
+            if (userTweets.get(userId) != null) {
+                tweetFeed.setTweets(new ArrayList<>(userTweets.get(userId)));
+            }
+            return tweetFeed;
+        } finally {
+            tweetsLock.readLock().unlock();
         }
-        return tweetFeed;
     }
 
     public void addTweet(String userId, Tweet tweet) {
-        if (userTweets.get(userId) == null) {
-            Set<Tweet> tweets = new TreeSet<>();
-            tweets.add(tweet);
-            userTweets.put(userId, tweets);
-        } else {
-            userTweets.get(userId).add(tweet);
+        tweetsLock.writeLock().lock();
+        try {
+            if (userTweets.get(userId) == null) {
+                Set<Tweet> tweets = new TreeSet<>();
+                tweets.add(tweet);
+                userTweets.put(userId, tweets);
+            } else {
+                userTweets.get(userId).add(tweet);
+            }
+        } finally {
+            tweetsLock.writeLock().unlock();
         }
     }
 
     public void followUser(String userId, String followedUser) {
-        if (userFollowers.get(userId) == null) {
-            Set<String> followedUsers = new HashSet<>();
-            followedUsers.add(followedUser);
-            userFollowers.put(userId, followedUsers);
-        } else {
-            userFollowers.get(userId).add(followedUser);
+        followersLock.writeLock().lock();
+        try {
+            if (userFollowers.get(userId) == null) {
+                Set<String> followedUsers = new HashSet<>();
+                followedUsers.add(followedUser);
+                userFollowers.put(userId, followedUsers);
+            } else {
+                userFollowers.get(userId).add(followedUser);
+            }
+        } finally {
+            followersLock.writeLock().unlock();
         }
     }
 
     public TweetFeed tweetFeed(String userId) {
-        Set<String> followedUsers = userFollowers.get(userId);
+        followersLock.readLock().lock();
+
+        Set<String> followedUsers;
+        try {
+            followedUsers = userFollowers.get(userId);
+        } finally {
+            followersLock.readLock().unlock();
+        }
 
         return feedFromFollowers(followedUsers);
     }
 
     private TweetFeed feedFromFollowers(Set<String> followedUsers) {
-        Set<Tweet> tweetSet = new TreeSet<>(followedUsers.stream().map(user -> userTweets.get(user))
-                        .flatMap(Collection::stream)
-                        .collect(Collectors.toSet()));
+        tweetsLock.readLock().lock();
+        try {
+            Set<Tweet> tweetSet = new TreeSet<>(followedUsers.stream().map(user -> userTweets.get(user))
+                            .flatMap(Collection::stream)
+                            .collect(Collectors.toSet()));
 
-        TweetFeed tweetFeed = new TweetFeed();
-        tweetFeed.setTweets(new ArrayList<>(tweetSet));
+            TweetFeed tweetFeed = new TweetFeed();
+            tweetFeed.setTweets(new ArrayList<>(tweetSet));
 
-        return tweetFeed;
+            return tweetFeed;
+        } finally {
+            tweetsLock.readLock().unlock();
+        }
     }
 }
